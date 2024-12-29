7cef9844354d19c3fc4ce19656228458f92da353
Victor Ekpo <algorithm.v@Mac.lan>
2024-12-28 20:13:29 -0600

    shell agent with gpt-4

--- src/agents/basic/shell_agent.py
+++ src/agents/basic/shell_agent.py
@@ -1,5 +1,6 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
+from phi.model.openai import OpenAIChat
 from phi.tools.shell import ShellTools
 from dotenv import load_dotenv
 
@@ -9,14 +10,14 @@ load_dotenv()
 
 # Initialize the agent with the model and tools
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile"),
+    # model=Groq(id="llama-3.3-70b-versatile"),
+    model=OpenAIChat(id="gpt-4"),
     tools=[ShellTools()],
     show_tool_calls=True,
+    instructions=["Use tables to display data."],
     markdown=False  # Turn off markdown
 )
 
 # Run the command using the agent
 def run():
-    response = agent.run("Show me the contents of the current directory by listing the files, only run one command")
-    print("Response", response)
-    print(format_output_to_table(response))
+    agent.print_response("Show me the contents of the current project by listing all files recursively, only run one command")
--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
 from src.agents.basic.finance_agent import run as finance_agent
 from src.agents.basic.shell_agent import run as shell_agent
 from src.agents.basic.research_agent import run as research_agent
 
 # finance_agent()
-# shell_agent()
+shell_agent()
 # research_agent()
